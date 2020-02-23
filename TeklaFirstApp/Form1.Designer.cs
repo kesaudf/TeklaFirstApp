62b5c0a6c89d2ba800cc86700052d1c1e79f24d7
kesaudf <alexapperwelt@mail.com>
2020-02-24 00:41:38 +0500

    Change Form

--- TeklaFirstApp/Form1.Designer.cs
+++ TeklaFirstApp/Form1.Designer.cs
@@ -31,10 +31,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.openDrawings = new System.Windows.Forms.CheckBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.createTopView = new System.Windows.Forms.CheckBox();
-            this.createFrontView = new System.Windows.Forms.CheckBox();
-            this.createEndView = new System.Windows.Forms.CheckBox();
             this.create3dView = new System.Windows.Forms.CheckBox();
+            this.createEndView = new System.Windows.Forms.CheckBox();
+            this.createFrontView = new System.Windows.Forms.CheckBox();
+            this.createTopView = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +44,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(69, 50);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Create";
+            this.button1.Text = "Создание";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -53,9 +53,9 @@
             this.openDrawings.AutoSize = true;
             this.openDrawings.Location = new System.Drawing.Point(12, 68);
             this.openDrawings.Name = "openDrawings";
-            this.openDrawings.Size = new System.Drawing.Size(99, 17);
+            this.openDrawings.Size = new System.Drawing.Size(96, 17);
             this.openDrawings.TabIndex = 1;
-            this.openDrawings.Text = "Open Drawings";
+            this.openDrawings.Text = "Открыть лист";
             this.openDrawings.UseVisualStyleBackColor = true;
             // 
             // groupBox1
@@ -69,47 +69,47 @@
             this.groupBox1.Size = new System.Drawing.Size(149, 111);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Views to be created";
-            // 
-            // createTopView
-            // 
-            this.createTopView.AutoSize = true;
-            this.createTopView.Location = new System.Drawing.Point(6, 19);
-            this.createTopView.Name = "createTopView";
-            this.createTopView.Size = new System.Drawing.Size(68, 17);
-            this.createTopView.TabIndex = 0;
-            this.createTopView.Text = "TopView";
-            this.createTopView.UseVisualStyleBackColor = true;
-            // 
-            // createFrontView
-            // 
-            this.createFrontView.AutoSize = true;
-            this.createFrontView.Location = new System.Drawing.Point(6, 42);
-            this.createFrontView.Name = "createFrontView";
-            this.createFrontView.Size = new System.Drawing.Size(73, 17);
-            this.createFrontView.TabIndex = 1;
-            this.createFrontView.Text = "FrontView";
-            this.createFrontView.UseVisualStyleBackColor = true;
-            // 
-            // createEndView
-            // 
-            this.createEndView.AutoSize = true;
-            this.createEndView.Location = new System.Drawing.Point(6, 65);
-            this.createEndView.Name = "createEndView";
-            this.createEndView.Size = new System.Drawing.Size(68, 17);
-            this.createEndView.TabIndex = 2;
-            this.createEndView.Text = "EndView";
-            this.createEndView.UseVisualStyleBackColor = true;
+            this.groupBox1.Text = "Типы видов";
             // 
             // create3dView
             // 
             this.create3dView.AutoSize = true;
             this.create3dView.Location = new System.Drawing.Point(6, 88);
             this.create3dView.Name = "create3dView";
-            this.create3dView.Size = new System.Drawing.Size(63, 17);
+            this.create3dView.Size = new System.Drawing.Size(62, 17);
             this.create3dView.TabIndex = 3;
-            this.create3dView.Text = "3DView";
+            this.create3dView.Text = "3D Вид";
             this.create3dView.UseVisualStyleBackColor = true;
+            // 
+            // createEndView
+            // 
+            this.createEndView.AutoSize = true;
+            this.createEndView.Location = new System.Drawing.Point(6, 65);
+            this.createEndView.Name = "createEndView";
+            this.createEndView.Size = new System.Drawing.Size(84, 17);
+            this.createEndView.TabIndex = 2;
+            this.createEndView.Text = "Задний вид";
+            this.createEndView.UseVisualStyleBackColor = true;
+            // 
+            // createFrontView
+            // 
+            this.createFrontView.AutoSize = true;
+            this.createFrontView.Location = new System.Drawing.Point(6, 42);
+            this.createFrontView.Name = "createFrontView";
+            this.createFrontView.Size = new System.Drawing.Size(97, 17);
+            this.createFrontView.TabIndex = 1;
+            this.createFrontView.Text = "Передний вид";
+            this.createFrontView.UseVisualStyleBackColor = true;
+            // 
+            // createTopView
+            // 
+            this.createTopView.AutoSize = true;
+            this.createTopView.Location = new System.Drawing.Point(6, 19);
+            this.createTopView.Name = "createTopView";
+            this.createTopView.Size = new System.Drawing.Size(97, 17);
+            this.createTopView.TabIndex = 0;
+            this.createTopView.Text = "Передний вид";
+            this.createTopView.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -120,7 +120,7 @@
             this.Controls.Add(this.openDrawings);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "BasicView";
+            this.Text = "Виды";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
